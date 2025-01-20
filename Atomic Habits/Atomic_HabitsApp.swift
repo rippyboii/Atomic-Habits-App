c2f6d6dc3fa18d6c0cce5a8ec12ba9cb096c940a
rippyboii <apeelsubedi@gmail.com>
2025-01-20 12:17:28 +0545

    Initial Commit

---
+++ Atomic Habits/Atomic_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atomic_HabitsApp.swift
+//  Atomic Habits
+//
+//  Created by Apeel Subedi on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Atomic_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atomic HabitsTests/Atomic_HabitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Atomic_HabitsTests.swift
+//  Atomic HabitsTests
+//
+//  Created by Apeel Subedi on 20/01/2025.
+//
+
+import Testing
+@testable import Atomic_Habits
+
+struct Atomic_HabitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
